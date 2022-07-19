6dcdadf21699df95c59bf1061e4c069b2d208ee9
apple <apple@apples-MacBook-Pro.local>
2022-07-19 19:58:36 +0300

    i finish all the task and bonus

--- pokemon/views.py
+++ pokemon/views.py
@@ -8,7 +8,9 @@ def get_pokemon (request,pokemon_id):
     return HttpResponse (f"{pokemons.name}\n {pokemons.type}\n {pokemons.hp}")
 
 
-
-
+def get_pokemons (request):
+    pokemonss = Pokemon.objects.all().values_list("name",flat=True)
+    Pokemon_list = "\n".join(f"<li>{pokemon}</li>" for pokemon in pokemonss)
+    return HttpResponse(f"<i>{Pokemon_list}</i>")
 
 
