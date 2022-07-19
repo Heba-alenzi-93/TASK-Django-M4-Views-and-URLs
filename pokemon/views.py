7b952acad34a3a4d44415a6dcd38a52c37ef9717
apple <apple@apples-MacBook-Pro.local>
2022-07-19 19:30:48 +0300

    i finish the task

--- pokemon/views.py
+++ pokemon/views.py
@@ -1,3 +1,14 @@
 from django.http import HttpResponse
-
 from .models import Pokemon
+
+
+def get_pokemon (request,pokemon_id):
+    pokemons = Pokemon.objects.get(id =pokemon_id )
+    print(pokemons)
+    return HttpResponse (f"{pokemons.name}\n {pokemons.type}\n {pokemons.hp}")
+
+
+
+
+
+
